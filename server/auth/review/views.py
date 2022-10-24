7c4f5718ce4c26f811108a453619b940b1cec07f
Napawan Nakasuwan <97944321+ampamp1a@users.noreply.github.com>
2022-10-24 21:04:12 +0700

    add

--- server/auth/review/views.py
+++ server/auth/review/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from django.http.response import JsonResponse
 
 # Create your views here.
