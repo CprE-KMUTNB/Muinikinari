25ebdcdde42d1bbe5e9f5037c934b064227e2cc1
Napawan Nakasuwan <97944321+ampamp1a@users.noreply.github.com>
2022-10-24 15:17:15 +0700

    add table

--- server/auth/random_menu/models.py
+++ server/auth/random_menu/models.py
@@ -3,13 +3,13 @@ from django.db import models
 # Create your models here.
 class Menu(models.Model):
     Menuid = models.AutoField(primary_key = True)
-    MenuName = models.CharField(max_lenght=500)
+    MenuName = models.CharField(max_length=500)
     MenuPrice = models.IntegerField()
     MenuPic = models.ImageField()
-    MenuType = models.CharField(max_lenght=500)
+    MenuType = models.CharField(max_length=500)
 
 class Restaurant(models.Model):
     Restaurantid = models.AutoField(primary_key = True)
-    RestaurantName =models.CharField(max_lenght=500)
+    RestaurantName =models.CharField(max_length=500)
     Menuid = models.IntegerField()
     RestaurantSeat = models.IntegerField()
--- server/auth/review/models.py
+++ server/auth/review/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Review(models.Model):
     Reviewid = models.AutoField(primary_key = True)
-    ReviewComment = models.CharField(max_lenght=1000)
+    ReviewComment = models.CharField(max_length=1000)
     id = models.IntegerField()
     ReviewDate = models.DateField
-    name = models.CharField(max_lenght=500)
+    name = models.CharField(max_length=500)
