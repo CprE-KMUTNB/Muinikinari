897d3747188b9d872c19f76afb714794e6554224
Napawan Nakasuwan <97944321+ampamp1a@users.noreply.github.com>
2022-10-24 14:58:16 +0700

    add table

--- server/auth/review/models.py
+++ server/auth/review/models.py
@@ -1,4 +1,9 @@
 from django.db import models
 
 # Create your models here.
-
+class Review(models.Model):
+    Reviewid = models.AutoField(primary_key = True)
+    ReviewComment = models.CharField(max_lenght=1000)
+    id = models.IntegerField()
+    ReviewDate = models.DateField
+    name = models.CharField(max_lenght=500)
