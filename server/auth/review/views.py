f0e5674ffe85778378bf3ec093ea406993aa461d
Napawan Nakasuwan <97944321+ampamp1a@users.noreply.github.com>
2022-10-27 12:11:21 +0700

    add

--- server/auth/review/views.py
+++ server/auth/review/views.py
@@ -9,5 +9,7 @@ from .models import Review
 class ReviewViewSet(viewsets.ModelViewSet):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
+#http://127.0.0.1:8000/apireview/review/id/delete
+        
 
 
