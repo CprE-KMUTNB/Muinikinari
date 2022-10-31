f12f16a3c1d9ced39c5c3490f712fab7a52f1026
Jarawee Trongsiriwut <97945209+tw94sh@users.noreply.github.com>
2022-10-31 17:41:41 +0700

    finished promotion

--- client/src/components/Slider.js
+++ client/src/components/Slider.js
@@ -2,10 +2,10 @@ import React from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import kahmoo from '../assets/kahmoo.jpg';
-import yumsalmon from '../assets/yumsalmon.jpg';
-import kapowmoo from '../assets/kapowmoo.jpg';
-import padseeew from '../assets/padseeew.jpg';
+import promotion1 from '../assets/promotion1.png';
+import promotion2 from '../assets/promotion2.png';
+import promotion3 from '../assets/promotion3.jpg';
+import promotion4 from '../assets/promotion4.jpg';
 import Scrollbar from 'smooth-scrollbar';
 import OverscrollPlugin from 'smooth-scrollbar/plugins/overscroll';
 import {useState,useEffect} from 'react';
@@ -26,40 +26,40 @@ function ImageSlider() {
         <div className='card-wrapper'>
         <div className='card'>
             <div className='card-image'>
-            <img src={kahmoo} alt='kahmoo' />
+            <img src={promotion1} alt='promotion1' />
             </div>
             <div className='details'>
-                <h2>ข้าวขาหมู <span className='restaurant-title'>ร้านสกล ศูนย์อาหาร ตึก 40 ขั้น 2 ช่อง 2</span></h2>
+                <h2>Food Wonderground<span className='restaurant-title'>คูปองส่วนลด 15% เมื่อผู้เข้าร่วมกิจกรรมมียอดสั่งซื้อขั้นต่ำ 500 บาท ต่อ 1 คำสั่งซื้อ</span></h2>
             </div>
         </div>
         </div>
         <div className='card-wrapper'>
         <div className='card'>
             <div className='card-image'>
-            <img src={yumsalmon} alt='yumsalmon' />
+            <img src={promotion2} alt='promotion2' />
             </div>
             <div className='details'>
-                <h2>ยำแซลม่อน <span className='restaurant-title'>ร้านเฮงเฮง ศูนย์อาหาร ตึก 40 ขั้น 2 ช่อง 9</span></h2>
+                <h2>FEEL GOOD FEEL FOO:D , พักร้อนนอนสบาย กินดีให้ฟีลกู๊ด<span className='restaurant-title'>ระหว่างวันที่ 17-26 ก.ย. ติดตาม WOW ITEMS ดีลเด็ดของอร่อย และโปรโรงแรม</span></h2>
             </div>
         </div>
         </div>
         <div className='card-wrapper'>
         <div className='card'>
             <div className='card-image'>
-            <img src={kapowmoo} alt='kapowmoo' />
+            <img src={promotion3} alt='promotion3' />
             </div>
             <div className='details'>
-                <h2>ข้าวกะเพราหมูสับ <span className='restaurant-title'>ร้านจารุณี ศูนย์อาหาร ตึก 40 ขั้น 2 ช่อง 5</span></h2>
+                <h2>สิทธิพิเศษ : เฉพาะสมาชิก MK Diamond and Black<span className='restaurant-title'>รับสิทธิ์ซื้อชุดอิ่มคุ้ม 899 บาท (จากปกติราคา 1,135 บาท)</span></h2>
             </div>
         </div>
         </div>
         <div className='card-wrapper'>
         <div className='card'>
             <div className='card-image'>
-            <img src={padseeew} alt='padseeew' />
+            <img src={promotion4} alt='promotion4' />
             </div>            
             <div className='details'>
-                <h2>ผัดซีอิ๊ว <span className='restaurant-title'>ร้านเฮงเฮง ศูนย์อาหาร ตึก 40 ขั้น 2 ช่อง 9</span></h2>
+                <h2>MK คุ้มตามใจ! ลดสูงสุดกว่า 30%* เริ่มต้นเพียง 299 บาท<span className='restaurant-title'>ให้คุณเลือกจัดเซตความอร่อยได้เอง จาก 4 หมวดสุดฮิต</span></h2>
             </div>
         </div>
         </div>
